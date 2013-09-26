a4ec063d38201bfa038c47f2e7da471944c0088e
Ben <Ben@Gardner-PC>
2013-09-26 16:37:02 -0500

    Program fully working. Modifications for flexibility may need to be made. Added documentation. Added validation

--- src/cashregister/LineItem.java
+++ src/cashregister/LineItem.java
@@ -1,44 +1,48 @@
 package cashregister;
 
 public class LineItem {
+
+    private final String INVALID_INPUT = "You have entered invalid input in the LineItem object";
     private Product product;
     private int itemQty = 0;
-
-    
     private StorageManager sm = new StorageManager();
-    
-    public LineItem(String productID, int qty){
-        //Need to validate input
-            sm.setStorageType(new FakeDatabaseReader());
-            Product p = sm.getProductById(productID);
-            addProduct(p);
-            setItemQty(qty);
-            
+
+    public LineItem(String productID, int qty) {
+        if (productID.length() == 0 || qty <= 0) {
+            throw new IllegalArgumentException(INVALID_INPUT);
+        }
+        sm.setStorageType(new FakeDatabaseReader());
+        Product p = sm.getProductById(productID);
+        addProduct(p);
+        setItemQty(qty);
+
     }
-    
-    public double returnDiscountedPrice(){
+
+    public double returnDiscountedPrice() {
         return product.returnDiscountedPrice();
     }
-    
-    public Product getCurrentLineProduct(){
+
+    public Product getCurrentLineProduct() {
         return product;
     }
-    
-    
-    
-    private void addProduct(Product p){
-        if(p != null){
-            product = p;
+
+    private void addProduct(Product p) {
+        if (p == null) {
+            throw new IllegalArgumentException(INVALID_INPUT);
         }
+        product = p;
+
     }
-    
+
     public int getItemQty() {
         return itemQty;
     }
 
     public void setItemQty(int itemQty) {
-        if (itemQty >= 1){
-            this.itemQty = itemQty;
+        if (itemQty <= 0) {
+            throw new IllegalArgumentException(INVALID_INPUT);
         }
+        this.itemQty = itemQty;
+
     }
 }
